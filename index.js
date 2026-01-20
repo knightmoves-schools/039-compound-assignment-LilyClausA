9649fda2104e7e8000cfe70d802bb8c88103f558
LilyClausA <lilcla1230@students.maquoketaschools.org>
2026-01-20 20:07:23 +0000

    la la la lala laaaaa

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+let addition = 50;
+addition = addition + 5;
+let subtraction = 40;
+subtraction = subtraction - 1;
+let multiplication = 5;
+multiplication = multiplication * 7;
+let division = 6;
+division = division / 2;
 
+document.getElementById("result").innerHTML = multiplication;
+
+
+//should assign 50 to a variable addition
+//should use compound assignment to add 5 to the addition
+//should assign 40 to a variable subtraction
+//should use compound assignment to subtract 1 from the subtraction
+//should assign 5 to a variable multiplication
+//should use compound assignment to multiply the multiplication by 7
+//should assign 6 to a variable division
+//should use compound assignment to divide the division by 2
+//should assign the innerHTML of the HTML element with the id result to the multiplication
